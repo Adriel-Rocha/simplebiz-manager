87dcf3b7c674e4c966f9e450a57fffacf4d8091f
Adriel Francisco Rocha <adrielfr194@gmail.com>
2026-01-08 20:55:16 -0300

    feat: add Client and Product entities

---
+++ src/main/java/com/adriel/simplebiz/simplebiz_manager/entity/Product.java
@@ -0,0 +1,27 @@
+package com.adriel.simplebiz.simplebiz_manager.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private String description;
+
+  private BigDecimal price;
+
+}
