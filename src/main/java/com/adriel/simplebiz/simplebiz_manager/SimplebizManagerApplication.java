b8e5d4f95b72c8d0ecb74b3057bb6677e88d4bd4
Adriel Francisco Rocha <adrielfr194@gmail.com>
2026-01-08 20:27:10 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/adriel/simplebiz/simplebiz_manager/SimplebizManagerApplication.java
@@ -0,0 +1,13 @@
+package com.adriel.simplebiz.simplebiz_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebizManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplebizManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adriel/simplebiz/simplebiz_manager/SimplebizManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriel.simplebiz.simplebiz_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplebizManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
