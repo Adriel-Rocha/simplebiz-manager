fbba2b00016f1f5f10bcf781bbc13d0c8f91f27a
Adriel Francisco Rocha <adrielfr194@gmail.com>
2026-01-08 22:47:55 -0300

    feat(security): implement UserDetailsService for authentication

---
+++ src/main/java/com/adriel/simplebiz/simplebiz_manager/service/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.adriel.simplebiz.simplebiz_manager.service;
+
+import com.adriel.simplebiz.simplebiz_manager.entity.User;
+import com.adriel.simplebiz.simplebiz_manager.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found")
+                );
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
