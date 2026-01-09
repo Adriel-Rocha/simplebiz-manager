3125b497a1c478a2774e1041ffad1dde6eb8d0f0
Adriel Francisco Rocha <adrielfr194@gmail.com>
2026-01-08 22:30:45 -0300

    refactor(user): improve entity mapping and constraints

--- src/main/java/com/adriel/simplebiz/simplebiz_manager/entity/User.java
+++ src/main/java/com/adriel/simplebiz/simplebiz_manager/entity/User.java
@@ -2,6 +2,8 @@ package com.adriel.simplebiz.simplebiz_manager.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,10 +26,12 @@ public class User {
   @Column(unique = true, nullable = false)
   private String email;
 
+  @Column(nullable = false)
   private String password;
 
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
   private Role role;
-
 
   public enum Role {
     ADMIN, USER
