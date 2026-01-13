3e3f97572a0298f21d4b22e83b06dc3bcbc5b50e
Adriel Francisco Rocha <adrielfr194@gmail.com>
2026-01-12 22:07:28 -0300

    feat: add authentication context and protected routes

---
+++ frontend/src/context/AuthProvider/index.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { AuthContext } from "./AuthContext";
+
+const token = localStorage.getItem("token");
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(token ? { token } : null);
+
+  const login = (token) => {
+    localStorage.setItem("token", token);
+    setUser({ token });
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isAuthenticated: !!user,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Dashboard from './pages/Dashboard'
+import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Dashboard />
+    <AuthProvider>
+      <Dashboard />
+    </AuthProvider>
   </StrictMode>,
 )
